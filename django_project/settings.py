10f67e87d5e5f54ef951fcaa6b11d4f3c9e9738e
Rahul Patel <patelr4142@gmail.com>
2020-08-31 12:48:09 +0530

    updated static root

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+Static_ROOT=os.path.join(BASE_DIR,'staticiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
